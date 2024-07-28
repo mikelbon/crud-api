e670780ac0e3d363caeb2101d6b7702a8a8920b9
Miguel Carhuas <mikel_ct@hotmail.com>
2024-07-28 13:07:04 -0500

    add Product

---
+++ src/main/java/com/pwm/aws/crud/lambda/api/model/Product.java
@@ -0,0 +1,15 @@
+package com.pwm.aws.crud.lambda.api.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+
+}
